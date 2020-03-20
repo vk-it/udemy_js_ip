7391900e6ca27ba298c3862fc55647410ca145bb
vk-it <46228289+vk-it@users.noreply.github.com>
2020-03-20 19:54:36 +0300

    Задание 3
    Начал выполнять задание 3

--- script.js
+++ script.js
@@ -12,22 +12,20 @@ time = prompt('Введите дату в формате YYYY-MM-DD', '2020-03-1
 let budget,
     timeData,
     expenses,
-    spending,
-    howMany,
     optionalExpenses,
     income,
     savings;
 
 time = timeData;
 
-spending = prompt('Введите обязательную статью расходов в этом месяце', '10 000 рублей');
-howMany = prompt('Во сколько обойдется?', '10 000 рублей');
-spending = prompt('Введите обязательную статью расходов в этом месяце', '10 000 рублей');
-howMany = prompt('Во сколько обойдется?', '10 000 рублей');
+let a1 = prompt('Введите обязательную статью расходов в этом месяце', '10 000 рублей'),
+    a2 = prompt('Во сколько обойдется?', '10 000 рублей'),
+    a3 = prompt('Введите обязательную статью расходов в этом месяце', '10 000 рублей'),
+    a4 = prompt('Во сколько обойдется?', '10 000 рублей');
 
 let appData = {
-    money,
-    timeData,
+    budget: money,
+    timeData: time,
 
     expenses: {
         '10 000 рублей' : '10 000 рублей'
@@ -37,4 +35,5 @@ let appData = {
     savings: false,
 };
 
-alert('Ваш бюджет на один день 1 500 рублей');
+// alert('Ваш бюджет на один день 1 500 рублей');
+alert(appData.budget / 30);
